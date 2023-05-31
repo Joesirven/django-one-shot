8d0bcd852809aa6a62c254aedc2c8920cc0b643d
Jose <jose@oldcutler.club>
2023-05-31 11:48:11 -0400

    Feature 5 Complete

--- todos/models.py
+++ todos/models.py
@@ -9,3 +9,22 @@ class TodoList(models.Model):
 
     def __str__(self):
         return self.name
+
+
+class TodoItem(models.Model):
+    task = models.CharField(max_length=100)
+    due_date = models.DateTimeField(
+        null=True,
+        blank=True,
+    )
+    is_completed = models.BooleanField(
+        default=False,
+    )
+    list = models.ForeignKey(
+        TodoList,
+        on_delete=models.CASCADE,
+        related_name="items",
+    )
+
+    def __str__(self):
+        return self.task
