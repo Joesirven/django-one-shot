b8be8f97a97e8fc7957bdfd1971bbb6bdf6bb008
Jose <jose@oldcutler.club>
2023-05-31 12:48:18 -0400

    Feature 7 Complete

---
+++ todos/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from todos.views import todos_list_list
+
+urlpatterns = [
+    path(
+        "",
+        todos_list_list,
+        name="todos_list",
+    ),
+]
--- todos/views.py
+++ todos/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_list_or_404
+from todos.models import TodoList, TodoItem
 
 # Create your views here.
+
+
+def todos_list_list(request):
+    todos = TodoList.objects.all()
+    context = {
+        "todos_list": todos,
+    }
+    return render(request, "todos/list.html", context)
